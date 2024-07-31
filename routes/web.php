89b153c10e1ed567efa210b040928abf89b5c33c
AlessiaLeci <alessia-black@live.it>
2024-07-31 12:05:20 +0200

    Modifiche

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RevisorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,4 +25,8 @@ Route::middleware('admin')->group(function () {
     Route::patch('/admin/{user}/set-admin', [AdminController::class, 'setAdmin'])->name('admin.setAdmin');
     Route::patch('/admin/{user}/set-revisor', [AdminController::class, 'setRevisor'])->name('admin.setRevisor');
     Route::patch('/admin/{user}/set-writer', [AdminController::class, 'setWriter'])->name('admin.setWriter');
+});
+
+Route::middleware('revisor')->group(function(){
+    Route::get('/revisor/dashboard', [RevisorController::class, 'dashboard'])->name('revisor.dashboard');
 });
