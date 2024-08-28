c4d5d26f6e48fd8575bc65dff168a0d48eee105f
AlessiaLeci <alessia-black@live.it>
2024-08-28 17:36:56 +0200

    User Story 7 completata

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,45 +5,52 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
-    use HasFactory, Searchable;
+    use HasFactory,Searchable;
+
 
     protected $fillable = [
-        'title', 
-        'subtitle', 
-        'body', 
-        'image', 
-        'user_id', 
-        'category_id', 
-        'is_accepted',
+        'title', 'subtitle', 'body', 'image', 'user_id', 'category_id', 'is_accepted', 'slug'
     ];
 
-    public function user()
-    {
+    public function user(){
         return $this->belongsTo(User::class);
     }
 
-    public function category()
-    {
+    public function category(){
         return $this->belongsTo(Category::class);
+        
     }
 
-    public function toSearchableArray()
-    {
-        return [
-            'id'       => $this->id,
-            'title'    => $this->title,
+    public function toSearchableArray(){
+
+        return[
+
+            'id' => $this->id,
+            'title' => $this->title,
             'subtitle' => $this->subtitle,
-            'body'     => $this->body,
+            'body' => $this->body,
             'category' => $this->category,
         ];
     }
-
     public function tags(){
         
         return $this->belongsToMany(Tag::class);
     }
-}
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function readDuration(){
+        $totalWords = Str::wordCount($this->body); //contiamo il numero delle parole
+        $minutesToRead = round($totalWords / 200); //arrotondiamo per eccesso i minuti
+        return intval($minutesToRead); //recupera il valore all'interno di una data variabile
+    }
+
+   
+}
